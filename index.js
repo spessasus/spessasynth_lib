cfe8d4cc67166be293441f50bf6f59895eb01b5d
spessasus <spesekspesek@gmail.com>
2025-04-14 19:38:04 +0200

    break everything

--- index.js
+++ index.js
@@ -1,31 +1,5 @@
 // Import modules
-import {
-    ALL_CHANNELS_OR_DIFFERENT_ACTION,
-    BasicInstrument,
-    BasicInstrumentZone,
-    BasicMIDI,
-    BasicPreset,
-    BasicPresetZone,
-    BasicSample,
-    BasicSoundBank,
-    ChannelSnapshot,
-    DEFAULT_PERCUSSION,
-    Generator,
-    IndexedByteArray,
-    loadSoundFont,
-    messageTypes,
-    MIDI,
-    MIDIBuilder,
-    midiControllers,
-    MIDIMessage,
-    Modulator,
-    modulatorSources,
-    NON_CC_INDEX_OFFSET,
-    RMIDINFOChunks,
-    SpessaSynthLogging,
-    SynthesizerSnapshot,
-    VOICE_CAP
-} from "spessasynth_core";
+
 import { Synthetizer } from "./synthetizer/synthetizer.js";
 import { Sequencer } from "./sequencer/sequencer.js";
 import { audioBufferToWav } from "./utils/buffer_to_wav.js";
@@ -39,39 +13,10 @@ export {
     // Synthesizer and Sequencer
     Sequencer,
     Synthetizer,
-    SynthesizerSnapshot,
-    ChannelSnapshot,
-    DEFAULT_PERCUSSION,
-    VOICE_CAP,
     DEFAULT_SYNTH_CONFIG,
-    ALL_CHANNELS_OR_DIFFERENT_ACTION,
-    NON_CC_INDEX_OFFSET,
-    
-    // SoundFont
-    BasicSoundBank,
-    BasicSample,
-    BasicInstrumentZone,
-    BasicInstrument,
-    BasicPreset,
-    BasicPresetZone,
-    Generator,
-    Modulator,
-    loadSoundFont,
-    modulatorSources,
-    
-    // MIDI
-    MIDI,
-    BasicMIDI,
-    MIDIBuilder,
-    MIDIMessage,
-    RMIDINFOChunks,
     
     // Utilities
-    IndexedByteArray,
     audioBufferToWav,
-    SpessaSynthLogging,
-    midiControllers,
-    messageTypes,
     MIDIDeviceHandler,
     WebMIDILinkHandler,
     WORKLET_URL_ABSOLUTE
