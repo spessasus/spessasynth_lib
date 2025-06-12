4ee623f794d82b3a01f82396ba8c75f505405a2b
spessasus <spesekspesek@gmail.com>
2025-06-12 02:38:49 +0200

    improve audio buffer to wav

--- src/utils/buffer_to_wav.js
+++ src/utils/buffer_to_wav.js
@@ -10,8 +10,8 @@ import { audioToWav } from "spessasynth_core";
 
 // noinspection JSUnusedGlobalSymbols
 /**
- *
- * @param audioBuffer {AudioBuffer}
+ * Converts an audio buffer into a wave file
+ * @param audioBuffer {AudioBuffer} variable channels
  * @param normalizeAudio {boolean} find the max sample point and set it to 1, and scale others with it
  * @param channelOffset {number} channel offset and channel offset + 1 get saved
  * @param metadata {WaveMetadata}
@@ -20,9 +20,16 @@ import { audioToWav } from "spessasynth_core";
  */
 export function audioBufferToWav(audioBuffer, normalizeAudio = true, channelOffset = 0, metadata = {}, loop = undefined)
 {
-    return new Blob([audioToWav({
-        leftChannel: audioBuffer.getChannelData(channelOffset),
-        rightChannel: audioBuffer.getChannelData(channelOffset + 1),
-        sampleRate: audioBuffer.sampleRate
-    }, normalizeAudio, metadata, loop)], { type: "audio/wav" });
+    /**
+     * @type {Float32Array[]}
+     */
+    const channels = [];
+    for (let i = 0; i < audioBuffer.numberOfChannels; i++)
+    {
+        channels.push(audioBuffer.getChannelData(channelOffset + i));
+    }
+    return new Blob(
+        [audioToWav(channels, audioBuffer.sampleRate, normalizeAudio, metadata, loop)],
+        { type: "audio/wav" }
+    );
 }
