ccf38de6ce02b2ddef0554262ab00abc4060356f
spessasus <spesekspesek@gmail.com>
2026-01-22 15:03:35 +0100

    build:fast

--- build_scripts/debug_enable.ts
+++ build_scripts/debug_enable.ts
@@ -2,6 +2,6 @@ import { runCommandSync } from "./run_command.ts";
 
 runCommandSync("npm uninstall spessasynth_core");
 runCommandSync("npm install ../spessasynth_core");
-runCommandSync("npm run build");
+runCommandSync("npm run build:fast");
 
 console.log("All commands completed successfully.");
