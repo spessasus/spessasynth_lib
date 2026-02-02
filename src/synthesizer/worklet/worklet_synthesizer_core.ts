1a76870aeef1aeccb7d00fd6920f3f8a91eec3ce
spessasus <spesekspesek@gmail.com>
2026-02-02 03:34:29 +0100

    4.1.2

--- src/synthesizer/worklet/worklet_synthesizer_core.ts
+++ src/synthesizer/worklet/worklet_synthesizer_core.ts
@@ -163,8 +163,8 @@ export class WorkletSynthesizerCore extends BasicSynthesizerCore {
             consoleColors.info
         );
         sq.loopCount = config.loopCount;
-        // Set voice cap to unlimited
-        this.synthesizer.setMasterParameter("voiceCap", Infinity);
+        // Set voice cap to pretty much unlimited
+        this.synthesizer.setMasterParameter("voiceCap", 10_000);
 
         /**
          * Set options
